b536ecdd257d7f6d575b0298a6aa12fb4eabb467
Gaurav Bagul <gauravsbagul@gmail.com>
2020-07-18 22:36:31 +0530

    root api done and pushed on ports 8005:8080

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const express = require('express');
+
 
 const DATABASE = "mongodb://mymongo:27017/dockertest";
+
+const app = express();
+
 mongoose
 .connect(DATABASE, {
     useNewUrlParser:true, 
@@ -12,3 +17,14 @@ mongoose
 }).catch(()=>{
     console.log('ERROR in DB Connection!',)
 });
+
+
+app.get('/', (req, res)=>{
+        res.json({
+            message:'Root route'
+        })
+});
+
+app.listen(8080, ()=>{
+    console.log('8080 is ready to serve...')
+})
